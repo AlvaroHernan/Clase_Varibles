4bb43ed1a0d07d9f0b641b895521c5386687a810
Alvaro Hernan Alarcon Lopez <alvaro.alarcon@corhuila.edu.co>
2020-08-24 04:19:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+#Python
+#Lenguaje programación interpretado
+#Orientado a objetos
+#No tipificado
+
+#Decimales, coma flotante, boolenos, cadenas, tuplas inmutables
+
+#listas, diccionarios
+#Variable es una etiqueta
+
+#Tipos de variable
+
+nota1=7.3     #Número entero
+print(nota1)
+nota2=5   #decimal o float
+print(nota2)
+nota=False   #decimal o float
+print(nota)
+nota="Primera clase"
+print(nota)
+nota_primer=5
+notaPrimer=6
+
+#Operaciones
+nota1=4
+nota2=3.6
+nota=nota1+nota2
+print(nota)
+nota=nota1/nota2
+print(nota)
+nota=nota1//nota2
+print(nota)
+
+list1=[nota,"Apellido","Nombre",57687,4.5]
+dato1=6
+dato1+=4
+dato2=dato1
+dato2*=7
+print(dato1)
+print(dato2)
+
+cad1="Carlos \n Tovar"
+print(cad1[0:6])
+"""
+cad2=""" es estudiante
+del programa
+de Ing Mecatronica """
+"""
+cad3=cad1+cad2
+print(cad3)
+cad3=cad3.lower()
+print(cad3)
+cad3=cad3.title()
+print(cad3)
+
+pos=cad3.find("a")
+print(pos)
+pos=cad3.count("a")
+print(pos)
+cad4=cad3.replace("a", "A")
+print(cad4)
+
+sensor=input("ingrese el valor de sensor: ")
+print(sensor)
+sensor=float(input("ingrese el valor de sensor: "))
+print(sensor)
